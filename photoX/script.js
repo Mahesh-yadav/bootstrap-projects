fe7f84a6f2925ed18114b550b609d39ae85a4d1d
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2020-07-15 08:57:43 +0530

    finish collection and gallery section photox

--- photoX/script.js
+++ photoX/script.js
@@ -23,3 +23,47 @@ window.addEventListener('scroll', () => {
         missionText.classList.remove('from-right');
     }
 });
+
+let currentFilter = 'all';
+
+const galleryTabs = document.querySelectorAll('.gallery-item');
+
+for(tab of galleryTabs){
+    tab.addEventListener('click', (e) => {
+
+        const previousTab = document.querySelector(`[data-filter=${currentFilter}]`);
+        previousTab.classList.remove('active-gallery-item');
+
+        currentFilter = e.target.dataset.filter;
+        e.target.classList.add('active-gallery-item');
+
+        displayFilteredSlides();
+    });
+}
+
+function displayFilteredSlides(){
+    const allImages = document.querySelectorAll('.filter');
+
+    for(image of allImages){
+        image.style.display = currentFilter === 'all' ? 'block' : 'none'; 
+    }
+
+    if(currentFilter === 'new'){
+        const newImages = document.querySelectorAll('.new');
+        for(img of newImages){
+            img.style.display = 'block'
+        }
+    }
+    else if(currentFilter === 'free'){
+        const freeImages = document.querySelectorAll('.free');
+        for(img of freeImages){
+            img.style.display = 'block'
+        }
+    }
+    else if(currentFilter === 'pro'){
+        const proImages = document.querySelectorAll('.pro');
+        for(img of proImages){
+            img.style.display = 'block'
+        }
+    }
+}
